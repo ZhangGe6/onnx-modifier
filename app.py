349ec7cbf62a4e710fbd76f741fc98bafc4740e3
lewis <luh.lewis@gmail.com>
2024-03-26 00:11:34 +0800

    [flake8] 1. make code style align to flake8
    2. avoid Bare except in #23.

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ import argparse
 from flask import Flask, render_template, request
 from onnx_modifier import onnxModifier
 
+
 app = Flask(__name__)
 onnx_modifier = None
+
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/open_model', methods=['POST'])
 def open_model():
@@ -17,10 +20,11 @@ def open_model():
     global onnx_modifier
     try:
         onnx_modifier = onnxModifier.from_name_protobuf_stream(onnx_file.filename, onnx_file.stream)
-    except:
+    except Exception:  # avoid bare exception, like SystemExit and KeyboardInterrupt
         onnx_modifier = onnxModifier.from_name_json_stream(onnx_file.filename, onnx_file.stream)
 
     return 'OK', 200
+
 
 def modify_and_download_model_in_format(ext):
     modify_info = request.get_json()
@@ -32,9 +36,11 @@ def modify_and_download_model_in_format(ext):
 
     return save_path
 
+
 @app.route('/download', methods=['POST'])
 def modify_and_download_onnx():
     return modify_and_download_model_in_format('.onnx')
+
 
 @app.route('/jsondownload', methods=['POST'])
 def modify_and_download_json():
@@ -54,9 +60,11 @@ def parse_args():
     args = parser.parse_args()
     return args
 
+
 def main():
     args = parse_args()
     app.run(host=args.host, port=args.port, debug=args.debug)
+
 
 if __name__ == '__main__':
     main()
